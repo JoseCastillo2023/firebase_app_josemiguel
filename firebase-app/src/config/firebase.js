526c911cf4912355fad75c986cb95c6956d48d57
JoseCastillo2023 <20220235@ricaldone.edu.sv>
2024-07-16 17:36:56 -0600

    Agregar Node

---
+++ firebase-app/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAmLutdVXeracYeRq7d51fL7dEVWGp0Bsg",
+  authDomain: "practica-firebase-20220235.firebaseapp.com",
+  projectId: "practica-firebase-20220235",
+  storageBucket: "practica-firebase-20220235.appspot.com",
+  messagingSenderId: "988840197208",
+  appId: "1:988840197208:web:a5df4593483af360885e2d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
